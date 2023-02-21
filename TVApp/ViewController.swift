131920af1040e86f81b25d86dc037cfa70c39ff3
Shota <sh.klibadze@gmail.com>
2023-02-21 10:12:01 +0400

    Initial Commit

---
+++ TVApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TVApp
+//
+//  Created by Shotiko Klibadze on 21.02.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
